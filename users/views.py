801fafee5a83f69b390d7bb00ebb4fb6745a67e2
xujie <2422640226@qq.com>
2020-12-09 20:04:09 +0800

    请求数据

--- users/views.py
+++ users/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        # 使用模板文件, 返回响应
+        return render(request, 'index.html')
+    else:
+        # 获取表单提交的用户名和密码
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print('username:{0}, passworld:{1}'.format(username, password))
+        # 进行业务处理
+        return HttpResponse('注册成功!')
